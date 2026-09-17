e5eec1dd665edbfdee2341ff3a1dd52f0bd41cd0
ardhitama <ardhitama@ymail.com>
2026-09-18 03:11:50 +0700

    Publish Ardcode v0.8.62 release metadata

--- Casks/ardcode.rb
+++ Casks/ardcode.rb
@@ -1,6 +1,6 @@
 cask "ardcode" do
-  version "0.8.61"
-  sha256 arm:          "a5210c8835d27f943ab644b15cb72d5073012d4ae1cfb256a7a009faed1e89db"
+  version "0.8.62"
+  sha256 arm:          "8f8197105d31b5d010fe4a2510d18c32f93a962954caf0ff606ff4108fdb2e2f"
   url "https://github.com/ardvis/homebrew-tap/releases/download/ardcode-v#{version}/ardcode-macos-arm64.tar.gz"
 
   depends_on macos: :tahoe
