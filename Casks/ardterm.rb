632ef6f74e74fedc7de2c5fadb4c3f47cb502de9
ardhitama <ardhitama@ymail.com>
2026-09-23 09:11:20 +0700

    Publish Ardterm v0.1.55 release metadata

--- Casks/ardterm.rb
+++ Casks/ardterm.rb
@@ -1,6 +1,6 @@
 cask "ardterm" do
-  version "0.1.54"
-  sha256 "d4078380d0f88a2eb16d7acf89fdfb7dc0121c42e0c6231e9c386392e1acc806"
+  version "0.1.55"
+  sha256 "4a53770ba94575f5498e58398ec254e303e5f3ad6b25eeb7a9da6bd894be6933"
   url "https://github.com/ardvis/homebrew-tap/releases/download/ardterm-v#{version}/Ardterm-macos-arm64.zip"
   name "Ardterm"
   desc "Native macOS terminal with authenticated remote sessions"
